bfe1d020fe051a7808ffdea68b6436a4bbe79774
Cyross Makoto <takuan45189@gmail.com>
2023-01-17 22:29:17 +0900

    added InsertWaveFileInNewAudioTrack method

--- VegasScriptPrescribedPattern/EntryPoint.cs
+++ VegasScriptPrescribedPattern/EntryPoint.cs
@@ -1,9 +1,5 @@
 ﻿using ScriptPortal.Vegas;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace VegasScriptPrescribedPattern
@@ -13,6 +9,22 @@ namespace VegasScriptPrescribedPattern
         public void FromVegas(Vegas vegas)
         {
             VegasHelper helper = VegasHelper.Instance(vegas);
+        }
+
+        /// <summary>
+        /// フォルダ選択ダイアログを開き、選択したフォルダの全wavファイルを新規オーディオファイルに貼り付ける
+        /// 貼り付け開始は現在のタイムルーラーの位置から。
+        /// </summary>
+        /// <param name="helper">VegasHelperオブジェクト</param>
+        /// <param name="initialFolder">参照開始するフォルダ。初期値はMyDocuments</param>
+        private void InsertWaveFileInNewAudioTrack(VegasHelper helper, Environment.SpecialFolder initialFolder = Environment.SpecialFolder.MyDocuments)
+        {
+            FolderBrowserDialog folderBrowser = new FolderBrowserDialog();
+            folderBrowser.RootFolder = initialFolder;
+            if (folderBrowser.ShowDialog() == DialogResult.OK )
+            {
+                helper.InseretAudioInTrack(folderBrowser.SelectedPath);
+            }
         }
     }
 }
